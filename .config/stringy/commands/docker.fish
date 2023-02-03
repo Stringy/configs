dafd2fe9e0da8e44efb20a43a2a32eb56059832e
Giles Hutton <hutton.giles@gmail.com>
2023-02-03 13:00:38 +0000

    Adds some helpful docker commands in fish

--- .config/stringy/commands/docker.fish
+++ .config/stringy/commands/docker.fish
@@ -9,3 +9,22 @@ function docker-bash --description "run bash in a given image"
 
     docker run -it --rm --entrypoint bash $argv[1]
 end
+
+function docker-push-ghutton --description "Retag and push an image for ghutton quay repo"
+    set -l image $argv[1]
+
+    set -l name_and_tag (basename $image)
+
+    docker tag $image quay.io/ghutton/$name_and_tag
+    docker push quay.io/ghutton/$name_and_tag
+end
+
+function docker-push-collector-ghutton --description "Push the latest collector image"
+    set -l tag (cd $GOPATH/src/github.com/stackrox/collector && make tag)
+    docker-push-ghutton quay.io/stackrox-io/collector:$tag
+end
+
+alias docker-image-names="docker images --format '{{.Repository}}:{{.Tag}}'"
+
+complete -x -c docker-bash --arguments "(docker-image-names | grep -v '<none>')"
+complete -x -c docker-push-ghutton --arguments "(docker-image-names | grep -v '<none>')"
