35e4deba3ca21b707d08dba8109a0eeba0e9a5ef
Giles Hutton <hutton.giles@gmail.com>
2024-03-21 14:46:33 +0000

    Add git functions, including rebase progress

---
+++ .config/stringy/commands/git.fish
@@ -0,0 +1,9 @@
+
+function git-rebase-progress
+    # ( RMD="$( git rev-parse --git-path 'rebase-merge/' )" && N=$( cat "${RMD}msgnum" ) && L=$( cat "${RMD}end" ) && echo "${N} / ${L}" ; )
+    # https://stackoverflow.com/a/57292015
+    set -l rmd (git rev-arse --git-path 'rebase-merge/')
+    set -l n "(cat $rmd"msgnum")"
+    set -l n "(cat $rmd"end")"
+    echo "$n / $l"
+end
