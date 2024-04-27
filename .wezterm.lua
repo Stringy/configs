ee3a84a72ed9c5989c5b44d797f4fc9a228a54ac
Stringy <giles@gileshutton.com>
2024-04-27 21:30:31 +0100

    fallback font for wezterm (basically linux support)

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font 'DejaVuSansMono Nerd Font'
+config.font = wezterm.font_with_fallback { 'DejaVuSansMono Nerd Font',  'DejaVuSansM Nerd Font Mono' }
+config.font_size = 11.0
 
 config.keys = {
     {
