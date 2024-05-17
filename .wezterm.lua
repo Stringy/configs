c2d1c97218de9992d77c95fa891a64bf815fc55e
Giles Hutton <hutton.giles@gmail.com>
2024-05-17 15:40:01 +0100

    light mode

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,8 +2,9 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font_with_fallback { 'DejaVuSansMono Nerd Font',  'DejaVuSansM Nerd Font Mono' }
+config.font = wezterm.font_with_fallback { 'DejaVuSansMono Nerd Font', 'DejaVuSansM Nerd Font Mono' }
 config.font_size = 11.0
+config.color_scheme = 'Catppuccin Latte'
 
 config.keys = {
     {
