c88cc209f09d67c6037f4f66d058c925a1414ff9
Giles Hutton <ghutton@redhat.com>
2025-09-09 16:16:37 +0100

    Adds Claude

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -6,54 +6,65 @@ elseif os.getenv('CODECOMPANION_URL') ~= nil then
 end
 
 return {
-    "olimorris/codecompanion.nvim",
-    config = true,
-    cond = function()
-        return adapter ~= nil
-    end,
-    opts = {
-        display = {
-            actions_palette = {
-                width = 95,
-                height = 10,
-                prompt = "Prompt ",                     -- Prompt used for interactive LLM calls
-                provider = "telescope",                 -- Can be "default", "telescope", or "mini_pick". If not specified, the plugin will autodetect installed providers.
+    {
+        "olimorris/codecompanion.nvim",
+        config = true,
+        cond = function()
+            return adapter ~= nil
+        end,
+        opts = {
+            display = {
+                actions_palette = {
+                    width = 95,
+                    height = 10,
+                    prompt = "Prompt ",                     -- Prompt used for interactive LLM calls
+                    provider = "telescope",                 -- Can be "default", "telescope", or "mini_pick". If not specified, the plugin will autodetect installed providers.
+                    opts = {
+                        show_default_actions = true,        -- Show the default actions in the action palette?
+                        show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+                    },
+                },
+            },
+            adapters = {
                 opts = {
-                    show_default_actions = true,        -- Show the default actions in the action palette?
-                    show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+                    show_defaults = false,
+                },
+                granite = function()
+                    if os.getenv('CODECOMPANION_URL') == nil then
+                        return nil
+                    end
+
+                    return require('codecompanion.adapters').extend('openai_compatible', {
+                        env = {
+                            url = os.getenv('CODECOMPANION_URL'),
+                        },
+                    })
+                end
+            },
+            strategies = {
+                chat = {
+                    adapter = adapter
+                },
+                inline = {
+                    adapter = adapter
+                },
+                cmd = {
+                    adapter = adapter
                 },
             },
         },
-        adapters = {
-            opts = {
-                show_defaults = false,
-            },
-            granite = function()
-                if os.getenv('CODECOMPANION_URL') == nil then
-                    return nil
-                end
-
-                return require('codecompanion.adapters').extend('openai_compatible', {
-                    env = {
-                        url = os.getenv('CODECOMPANION_URL'),
-                    },
-                })
-            end
-        },
-        strategies = {
-            chat = {
-                adapter = adapter
-            },
-            inline = {
-                adapter = adapter
-            },
-            cmd = {
-                adapter = adapter
-            },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
         },
     },
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-    },
+    {
+        "greggh/claude-code.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- Required for git operations
+        },
+        config = function()
+            require("claude-code").setup()
+        end
+    }
 }
--- .config/stringy/commands/ai.fish
+++ .config/stringy/commands/ai.fish
@@ -3,3 +3,7 @@ function gemini-ai-key
     set -l key (gcloud services api-keys get-key-string --project acs-senseco-ai-playground bdc2ab7d-5484-4e9a-8e9c-1989036097c3 | string split ' ' -f2)
     set -gx GEMINI_API_KEY $key
 end
+
+set -gx CLAUDE_CODE_USE_VERTEX 1
+set -gx CLOUD_ML_REGION us-east5
+set -gx ANTHROPIC_VERTEX_PROJECT_ID itpc-gcp-hybrid-pe-eng-claude
